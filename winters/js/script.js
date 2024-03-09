17af3f08f07301349baa678f178d8e066f2d9651
NGNikolay <georgiev.nikolaj@gmail.com>
2024-03-09 14:23:43 +0200

    removed some console logs

--- winters/js/script.js
+++ winters/js/script.js
@@ -2,9 +2,7 @@ const checkbox = document.querySelector('.navigation__checkbox');
 const navLinks = document.querySelector('.navigation__list');
 navLinks.addEventListener('click', (e) => {
   const navLinkClass = e.target.classList[0];
-  console.log(navLinkClass);
   if (navLinkClass === 'navigation__link') {
     checkbox.checked = false;
-    console.log(checkbox);
   }
 })
